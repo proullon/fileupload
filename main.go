eaa6bbacfd43328142ea059022175fbe5f673187
Pierre Roullon <pierre.roullon@gmail.com>
2015-12-16 22:39:47 +0100

    test: upload and see

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
-	"crypto/md5"
 	"fmt"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
-	"strconv"
-	"time"
 )
 
+const ImageDirectory = "./test"
+
 func main() {
+
+	// home page for example
+	http.HandleFunc("/", home)
+
+	// Upload handler
 	http.HandleFunc("/upload", upload)
+
+	// Directory where pictures are stored
+	fs := http.FileServer(http.Dir(ImageDirectory))
+	http.Handle("/images/", http.StripPrefix("/images/", fs))
 
 	err := http.ListenAndServe(":8082", nil)
 	if err != nil {
@@ -20,32 +29,57 @@ func main() {
 	}
 }
 
+func home(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("upload.gtpl")
+
+	var images []string
+	fis, err := ioutil.ReadDir(ImageDirectory)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	for _, fi := range fis {
+		images = append(images, fi.Name())
+	}
+
+	err = t.Execute(w, images)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
 // upload logic
 func upload(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method)
-	if r.Method == "GET" {
-		crutime := time.Now().Unix()
-		h := md5.New()
-		io.WriteString(h, strconv.FormatInt(crutime, 10))
-		token := fmt.Sprintf("%x", h.Sum(nil))
-
-		t, _ := template.ParseFiles("upload.gtpl")
-		t.Execute(w, token)
-	} else {
-		r.ParseMultipartForm(32 << 20)
-		file, handler, err := r.FormFile("uploadfile")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer file.Close()
-		fmt.Fprintf(w, "%v", handler.Header)
-		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer f.Close()
-		io.Copy(f, file)
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", 302)
+		return
 	}
+	fmt.Println("method:", r.Method)
+
+	// Get file handler from http request
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("uploadfile")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Open file on disk
+	f, err := os.OpenFile(ImageDirectory+"/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	// We copy the file content from http request to disk
+	io.Copy(f, file)
+
+	// Done, now we redirect user to home page
+	http.Redirect(w, r, "/", 302)
 }
--- upload.gtpl
+++ upload.gtpl
@@ -5,9 +5,16 @@
 <body>
 <form enctype="multipart/form-data" action="/upload" method="post">
       <input type="file" name="uploadfile" />
-      <input type="hidden" name="token" value="{{.}}"/>
       <input type="submit" value="upload" />
 </form>
+
+
+{{ range . }}
+<p>
+<img src="/images/{{.}}">
+</p>
+{{end}}
+
 </body>
 </html>
 
